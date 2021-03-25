d1e69bc78f5dbdd2b6483a7ac3b7e040f130e217
Pau Abella <pauabellamolina1@gmail.com>
2021-03-25 22:49:43 +0100

    Showing maps centered to users location

--- src/components/GMap.vue
+++ src/components/GMap.vue
@@ -18,12 +18,14 @@ export default {
 
         onBeforeMount(() => {
             // const key = process.env.VUE_APP_GOOGLEMAPS_KEY;
-            const key = 'AIzaSyCWkXP_eBF1sPXKybaQ2ptmhb-fC_xJGig';
+            
+            const darkMapId = 'fff522f093b3acd0'; //dark map
+            const lightMapId = 'a2fc72d1ec9d4d81'; //light map
 
             const googleMapScript = document.createElement("SCRIPT");
             googleMapScript.setAttribute(
                 "src",
-                `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`
+                `https://maps.googleapis.com/maps/api/js?key=${key}&map_ids=${darkMapId}&callback=initMap`
             );
             googleMapScript.setAttribute("defer", "");
             googleMapScript.setAttribute("async", "");
@@ -32,7 +34,7 @@ export default {
 
         window.initMap = () => {
             map.value = new window.google.maps.Map(mapDivRef.value, {
-                mapTypeId : "roadmap",
+                mapId: "fff522f093b3acd0",
                 zoom: props.zoom || 8,
                 disableDefaultUI: true,
                 center: props.center || { lat: 38.0, lng: -77.01 }
--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,8 +11,9 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-  
-      <g-map :zoom="5"></g-map>
+
+      <g-map :zoom="15" v-if="loc" :center="{ lat: loc?.coords.latitude, lng: loc?.coords.longitude }"></g-map>
+      <g-map :zoom="7" v-if="!loc" :center="{ lat: 41.15612, lng: 1.10687 }"></g-map>
       <!-- <ExploreContainer name="Tab 1 page" /> -->
     </ion-content>
   </ion-page>
@@ -21,9 +22,25 @@
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import GMap from "../components/GMap.vue";
+import { Plugins } from '@capacitor/core'
+import { defineComponent } from 'vue'
 
-export default  {
+export default defineComponent ({
   name: 'Tab1',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, GMap },
-}
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, GMap},
+  data() {
+    return{ loc: null as any }
+  },
+  methods: {
+    async getCurrentPosition(){
+      const { Geolocation } = Plugins;
+      const loc = await Geolocation.getCurrentPosition()
+      setTimeout(() => this.loc = loc, 5000);
+      // this.loc = loc
+    }
+  },
+  mounted () {
+    this.getCurrentPosition()
+  }
+})
 </script>
