604d5ccf1542305a3aa2059daf115f399c114aba
Pau Abella <pauabellamolina1@gmail.com>
2021-05-12 23:32:34 +0200

    Added the use recompense button, the already used recompense logic and the qr generation for the recompense use submit.

--- src/components/CommerceComponentRecompense.vue
+++ src/components/CommerceComponentRecompense.vue
@@ -1,36 +1,75 @@
 <template>
     <div style="display: flex;" class="commerceBox ion-justify-content-center ion-align-items-center">
-        <ion-grid>
-          <ion-row class="h-100 p-5 ion-justify-content-center">
-            <ion-col size="4" style="display: flex;" class="ion-justify-content-center ion-align-items-center">
-                <div class="commerceImgParent">
-                    <img class="commerceImg" :src="'data:image/png;base64,'+recompense.picture" :alt="recompense.title" />
+        <ion-grid style="margin: 0; width: 100%;">
+            <ion-row style="background-color: white;" class="h-100 p-5 ion-justify-content-center">
+                <ion-col size="4" style="display: flex;" class="ion-justify-content-center ion-align-items-center">
+                    <div class="commerceImgParent">
+                        <img class="commerceImg" :src="'data:image/png;base64,'+recompense.picture" :alt="recompense.title" />
+                    </div>
+                </ion-col>
+                <ion-col size="8" class="recompenseInfo">
+                    <div>
+                        <h6>{{ recompense.title }}</h6>
+                        <p>{{ recompense.description }}</p>
+                    </div>
+                    <div><span class="points">{{ recompense.points }}</span><span> punts</span></div>
+                </ion-col>
+            </ion-row>
+            <ion-row style="display: flex;" class="ion-justify-content-center">
+                <div class="pointsButtonInfo">
+                    <ion-button v-if="recompense.used == true && recompense.active == 1" class="useRecompense" style="--backgound: #464646;" disabled>Ja utilitzada</ion-button>
+                    <ion-button v-if="recompense.used == false && recompense.active == 1" @click="useRecompense()" class="useRecompense" style="--backgound: #464646;">{{ showHide }} QR</ion-button>
+                    <ion-button v-if="recompense.active == 0" class="useRecompense" style="--backgound: #464646;" disabled>No disponible</ion-button>
+                    <transition name="slide">
+                        <img style="z-index: -1;" v-if="showQr" :src="'https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=210x210&chld=L|1&chl='+userId+'/'+recompense.id">
+                    </transition>
                 </div>
-            </ion-col>
-            <ion-col size="8">
-                <h6>{{ recompense.title }}</h6>
-                <span>{{ recompense.points }}</span>
-                <p>{{ recompense.description }}</p>
-            </ion-col>
-          </ion-row>
+            </ion-row>
         </ion-grid>
     </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue"
+import { ENV } from "@/enviroments/enviroment";
 
-export default {
+export default defineComponent({
   name: 'CommerceComponentRecompense',
   props: {
     recompense: Object //Commerce id received from GMap
   },
-}
+  data() {
+      return {
+          showQr: false,
+          showHide: "Veure",
+          userId: ENV.userId,
+      }
+  },
+  methods: {
+      useRecompense() {
+          this.showQr = !this.showQr;
+          this.showHide == "Amagar" ? this.showHide = "Veure" : this.showHide = "Amagar";
+      }
+  }
+})
 </script>
 
 <style scoped>
+.slide-enter-active {
+  transition: all .1s;
+}
+
+.slide-leave-active {
+  transition: all .1s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(-10em);
+}
+
 .commerceBox {
     width: 100%;
-    height: 120px;
     background-color: white;
     opacity: 0.85;
     /* background-color: #C4C4C4; */
@@ -86,17 +125,44 @@ h6::-webkit-scrollbar {
     width: 0 !important;
 }
 
-span {
-    font-size: 12px;
-    color: rgb(63, 63, 63);
-    font-weight: 500;
-    margin: 0 0 0 8px;
-}
-
 p {
-    font-size: 14px;
-    margin: 3px 0 0 0;
+    font-size: .9em;
+    margin: 0 0 0 .3em;
     font-weight: 600;
     color: rgb(29, 29, 29);
+}
+
+.recompenseInfo {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+}
+.pointsButtonInfo {
+    width: 95%;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+    align-items: center;
+}
+    .useRecompense {
+        width: 100%;
+        height: 3em;
+        background-color: white;
+        margin-top: 0;
+        --border-radius: 10px;
+        --background: #313131;
+        --color: white;
+    }
+
+.points {
+    font-family: 'Rubik Mono One', sans-serif;
+    font-size: 1.6em;
+    font-size: 2em;
+    font-weight: 400;
+}
+span {
+    font-size: 1em;
+    /* color: rgb(63, 63, 63); */
+    font-weight: 900;
 }
 </style>
