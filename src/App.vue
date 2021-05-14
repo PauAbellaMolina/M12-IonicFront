d1e49b6c38312bcf86b965399f077f513bf5fa2a
Pau Abella <pauabellamolina1@gmail.com>
2021-05-14 17:50:39 +0200

    WIP Push notifications

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,53 @@
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { FCM } from '@capacitor-community/fcm';
+import { Plugins } from '@capacitor/core';
+
+ const { PushNotifications } = Plugins;
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
-  }
+  },
+  data() {
+    return {
+      fcm: new FCM()
+    }
+  },
+  mounted() {
+    PushNotifications.requestPermission();
+    // .then(result => {
+    //   alert("result " + JSON.stringify(result));
+    // });
+            
+    // PushNotifications.addListener("registrationError", (error) => {
+    //   console.log(`error on register ${JSON.stringify(error)}`);
+    // }),
+
+    //   PushNotifications.addListener(
+    //     "pushNotificationReceived",
+    //     (notification) => {
+    //       console.log(`notification ${JSON.stringify(notification)}`);
+    //     }
+    //   ),
+      
+    //   PushNotifications.addListener(
+    //     "pushNotificationActionPerformed",
+    //     async (notification) => {
+    //         alert("notification " + notification)
+    //       console.log("notification succeeded");
+    //     }
+    //   ),
+      
+      PushNotifications.register();
+      
+    const fcmToken = this.fcm.getToken();
+    alert(JSON.stringify(fcmToken));
+    console.log("token:" + JSON.stringify(fcmToken));
+  },
 });
 </script>
 <style>
