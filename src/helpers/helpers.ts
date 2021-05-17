5bd34950aa67c508525311ee50b20cdadfa52179
Pau Abella <pauabellamolina1@gmail.com>
2021-05-18 01:58:52 +0200

    Added all the icons and markers of the final app

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -3,6 +3,10 @@ class Helpers {
         //Importing the different types of markers
         const coffeeMarker = "/assets/markers/coffee_marker.svg";
         const cupcakeMarker = "/assets/markers/cupcake_marker.svg";
+        const beerMarker = "/assets/markers/beer_marker.svg";
+        const cdMarker = "/assets/markers/cd_marker.svg";
+        const restaurantMarker = "/assets/markers/restaurant_marker.svg";
+        const speakerMarker = "/assets/markers/speaker_marker.svg";
         //Depending on the category received set one marker or another
         switch (category) {
             case 1:
@@ -10,6 +14,18 @@ class Helpers {
                 break;
             case 2:
                 return cupcakeMarker;
+                break;
+            case 3:
+                return beerMarker;
+                break;
+            case 4:
+                return restaurantMarker;
+                break;
+            case 5:
+                return speakerMarker;
+                break;
+            case 6:
+                return cdMarker;
                 break;
             default:
                 break;
@@ -18,15 +34,31 @@ class Helpers {
 
     getIcon(category: number) {
         //Importing the different types of markers
-        const coffeeMarker = "/assets/icons/coffee_icon.svg";
-        const cupcakeMarker = "/assets/icons/cupcake_icon.svg";
+        const coffeeIcon = "/assets/icons/coffee_icon.svg";
+        const cupcakeIcon = "/assets/icons/cupcake_icon.svg";
+        const beerIcon = "/assets/icons/beer_icon.svg";
+        const cdIcon = "/assets/icons/cd_icon.svg";
+        const restaurantIcon = "/assets/icons/restaurant_icon.svg";
+        const speakerIcon = "/assets/icons/speaker_icon.svg";
         //Depending on the category received set one marker or another
         switch (category) {
             case 1:
-                return coffeeMarker;
+                return coffeeIcon;
                 break;
             case 2:
-                return cupcakeMarker;
+                return cupcakeIcon;
+                break;
+            case 3:
+                return beerIcon;
+                break;
+            case 4:
+                return restaurantIcon;
+                break;
+            case 5:
+                return speakerIcon;
+                break;
+            case 6:
+                return cdIcon;
                 break;
             default:
                 break;
